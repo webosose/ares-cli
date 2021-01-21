a754752550fee5fac9599163f957ab1a91c55546
ye.kim <ye0607.kim@lge.com>
2021-01-21 14:13:39 +0900

    Support capture screen (#11)
    :Release Notes:
    Support capture in ares-device
    
    :Detailed Notes:
    Implement capture screen feature to developer watch target display
    
     Add ares-device --capture-screen(-c)
     Add capture option description in ares-device help
     Update positive/negative TC for capture option
    
    :Testing Performed:
    
    Pass unit test on ose and auto
    Pass eslint
    Verify with CLI commands
    
    Check capture file generated with targetname__display0__time.png
    $ ./ares-device.js -c
    Create rpi4_display0_20210115095912.png to /home/ye0607kim/project/github/ares-cli/bin
    Success
    
    Give display option and capture file name should be targetname__display1__time.png
    $./ares-device.js -c -dp 1
    Create rpi4_display1_20210115095826.png to /home/ye0607kim/project/github/ares-cli/bin
    Success
    
    Give directory path with display option
    $ ./ares-device.js -c webOSCap -dp 1
    Create rpi4_display1_20210115095944.png to /home/ye0607kim/project/github/ares-cli/bin/webOSCap
    Success
    
    Give file name and format(png, bmp, jpg) and check the generated file name & format
    $ ./ares-device.js -c webOSCap/test1.bmp [or test1.jpg or test1.png]
    Create test1.bmp [or test1.jpg or test1.png] to /home/ye0607kim/project/github/ares-cli/bin/webOSCap
    Success
    
    Check /tmp/aresCapture has been removed from device target after command working completed.
    
    :Issues Addressed:
    [PLAT-125564] Implement ares-device --capture-screen

--- spec/jsSpecs/ares-device.spec.js
+++ spec/jsSpecs/ares-device.spec.js
@@ -5,7 +5,12 @@
  */
 
 const exec = require('child_process').exec,
+    path = require('path'),
+    fs = require('fs'),
     common = require('./common-spec');
+
+const captureDirPath = path.join(__dirname, "..", "tempFiles", "deviceCapture"),
+    dateFileReg = new RegExp("[A-Za-z0-9]*_display[0-9]_[0-9]*.png");
 
 const aresCmd = 'ares-device';
 
@@ -91,6 +96,139 @@ describe(aresCmd, function() {
                 keys.forEach(function(key) {
                     expect(stdout).toContain(key);
                 });
+            }
+            done();
+        });
+    });
+});
+
+describe(aresCmd + ' --capture-screen(-c)', function() {
+    let generatedFile = "";
+
+    beforeEach(function(done) {
+        generatedFile = "";
+        common.removeOutDir(captureDirPath);
+        done();
+    });
+    afterEach(function(done) {
+        common.removeOutDir(generatedFile);
+        common.removeOutDir(captureDirPath);
+        done();
+    });
+
+    it('Capture', function(done) {
+        exec(cmd + ` -c`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain(options.device);
+            expect(stdout).toContain(new Date().getFullYear());
+            expect(stdout).toContain("display0");
+            expect(stdout).toContain(".png");
+            expect(stdout).toContain(path.resolve('.'));
+
+            const matchedFiles = stdout.match(dateFileReg);
+
+            generatedFile = path.join(path.resolve('.'), matchedFiles[0]);
+            console.log("capture file name : " + matchedFiles[0]);
+            expect(fs.existsSync(generatedFile)).toBe(true);
+            done();
+        });
+    });
+
+    it('Capture with filename', function(done) {
+        exec(cmd + ` -c screen.png`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).not.toContain(options.device);
+            expect(stdout).not.toContain("display0");
+            expect(stdout).toContain("screen.png");
+            expect(stdout).toContain(path.resolve('.'));
+
+            generatedFile = path.join(path.resolve('.'), "screen.png");
+            expect(fs.existsSync(generatedFile)).toBe(true);
+            done();
+        });
+    });
+
+    it('Capture with directory Path & display option', function(done) {
+        exec(cmd + ` -c ${captureDirPath} --display 1`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain(options.device);
+            expect(stdout).toContain(new Date().getFullYear());
+            expect(stdout).toContain("display1");
+            expect(stdout).toContain(".png");
+            expect(stdout).toContain(captureDirPath);
+            expect(fs.existsSync(captureDirPath)).toBe(true);
+
+            const matchedFiles = stdout.match(dateFileReg);
+
+            console.log("capture file name : " + matchedFiles[0]);
+            expect(fs.existsSync(path.join(captureDirPath, matchedFiles[0]))).toBe(true);
+            done();
+        });
+    });
+
+    it('Capture with directory & file path(bmp)', function(done) {
+        const captureFilePath = path.join(captureDirPath, "screen.bmp");
+        exec(cmd + ` -c ${captureFilePath}`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).not.toContain(options.device);
+            expect(stdout).not.toContain("display0");
+            expect(stdout).toContain("screen.bmp");
+            expect(stdout).toContain(captureDirPath);
+            expect(fs.existsSync(captureDirPath)).toBe(true);
+            done();
+        });
+    });
+
+    it('Capture with directory & file path(jpg)', function(done) {
+        const captureFilePath = path.join(captureDirPath, "screen.jpg");
+        exec(cmd + ` -c ${captureFilePath}`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).not.toContain(options.device);
+            expect(stdout).not.toContain("display0");
+            expect(stdout).toContain("screen.jpg");
+            expect(stdout).toContain(captureDirPath);
+            expect(fs.existsSync(captureDirPath)).toBe(true);
+            done();
+        });
+    });
+});
+
+describe(aresCmd + ' negative TC', function() {
+    it('Capture with invalid file format', function(done) {
+        exec(cmd + ` -c "test.abc"`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+                expect(stderr).toContain("Please specify file extension, either 'png' 'bmp' or 'jpg'");
+            }
+            done();
+        });
+    });
+
+    it('Capture with invalid destiation Path', function(done) {
+        exec(cmd + ` -c /rootDir`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+                expect(stderr).toContain("permission denied, mkdir '/rootDir'");
+            }
+            done();
+        });
+    });
+
+    it('Capture with invalid display ID', function(done) {
+        exec(cmd + ` -c --display 10`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+                expect(stderr).toContain("luna-send command failed <ERR_INVALID_DISPLAY>");
             }
             done();
         });
