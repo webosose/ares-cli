e7cd6259d580d79e0d35b02636c38959a9dfe5b3
ssuminahn <sumin.ahn@lge.com>
2021-05-07 13:44:33 +0900

    :Release Notes: Support ares-log
    :Detailed Notes:
    ares-log provides the user with a more convenient function to check the log.
    Most of the options provided by the existing journald can be utilized.
    In addition, it provides a function to save log, search log file list, and search unit list.
    
    :Testing Performed:
    1. Pass unit test on ose and auto
    2. Pass eslint
    3. Check below options
    - node ares-log.js -sd pmlogd
    - node ares-log.js -cd
    - node ares-log.js -f
    - node ares-log.js -r
    - node ares-log.js -n 7
    - node ares-log.js -k
    - node ares-log.js -b
    - node ares-log.js -S today
    - node ares-log.js -U yesterday
    - node ares-log.js -S 2021-04-07 21:45:00 --until 2021-04-07 21:46:00
    - node ares-log.js -f -pid 1834
    - node ares-log.js -f -o json
    - node ares-log.js -n 7 -s
    - node ares-log.js -f -s
    - node ares-log.js -s tmp.log
    - node ares-log.js -s tmp
    - node ares-log.js -f -o -d ose
    - node ares-log.js -f -o jjson -d ose
    - node ares-log.js --file-list
    - node ares-log.js --file system.journal
    -  node ares-log.js -ul
    - node ares-log.js -ul -dp 1
    - node ares-log.js -u com.webos.service.position -d auto
    - node ares-log.js -u sam -dp 1 -f -d auto
    
    :Issues Addressed:
    [PLAT-134677] Support to unit test of ares-log

---
+++ bin/ares-log.js
@@ -0,0 +1,228 @@
+#!/usr/bin/env node
+
+/*
+ * Copyright (c) 2021 LG Electronics Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const path = require('path'),
+    log = require('npmlog'),
+    nopt = require('nopt'),
+    async = require('async'),
+    logLib = require('./../lib/log'),
+    commonTools = require('./../lib/base/common-tools');
+
+const version = commonTools.version,
+    cliControl = commonTools.cliControl,
+    help = commonTools.help,
+    appdata = commonTools.appdata,
+    setupDevice = commonTools.setupDevice,
+    errHndl = commonTools.errMsg;
+
+const processName = path.basename(process.argv[1]).replace(/.js/, '');
+
+process.on('uncaughtException', function (err) {
+    log.error('uncaughtException', err.toString());
+    log.verbose('uncaughtException', err.stack);
+    cliControl.end(-1);
+});
+
+if (process.argv.length === 2) {
+    process.argv.splice(2, 0, '--help');
+}
+
+const knownOpts = {
+    "help":     Boolean,
+    "version":  Boolean,
+    "level":    ['silly', 'verbose', 'info', 'http', 'warn', 'error'],
+    "device":   [String, null],
+    "device-list":  Boolean,
+    // command options
+    "switch-daemon":   String,
+    "current-daemon":   Boolean,
+    // show options
+    "lines":    Number,
+    "reverse":  Boolean,
+    "follow":   Boolean,
+    // filter options
+    "since":    String,
+    "until":    String,
+    "priority": String,
+    "kernel":   Boolean,
+    "boot":     Boolean,
+    "pid":      Number,
+    // output option
+    "output":   Boolean,
+    // save option
+    "save":     Boolean,
+    // read options
+    "file": String,
+    "file-list": Boolean,
+    // unit options
+    "unit":     String,
+    "unit-list": Boolean,
+    "display": [String, null]
+};
+
+const shortHands = {
+    // generic aliases
+    "h": ["--help"],
+    "V": ["--version"],
+    "v": ["--level", "verbose"],
+    "d": ["--device"],
+    "D": ["--device-list"],
+    "sd": ["--switch-daemon"],
+    "cd": ["--current-daemon"],
+    "n": ["--lines"],
+    "r": ["--reverse"],
+    "f": ["--follow"],
+    "S": ["--since"],
+    "U": ["--until"],
+    "p": ["--priority"],
+    "k": ["--dmesg"],
+    "b": ["--boot"],
+    "pid": ["--pid"],
+    "o": ["--output"],
+    "s": ["--save"],
+    "file": ["--file"],
+    "fl": ["--file-list"],
+    "u" : ["--unit"],
+    "ul" : ["--unit-list"],
+    "dp" : ["--display"]
+};
+
+const argv = nopt(knownOpts, shortHands, process.argv, 2 /* drop 'node' & 'ares-*.js'*/);
+
+log.heading = processName;
+log.level = argv.level || 'warn';
+log.verbose("argv", argv);
+
+const options = {
+    device: argv.device,
+    display: argv.display,
+    argv: argv
+};
+
+const pmLogOptions = ["follow", "reverse", "lines", "priority", "save", "display", "level", "device"],
+    journalLogOptions = ["follow", "reverse", "lines", "since", "until", "pid", "dmesg", "boot", "output", "file",
+                        "priority", "save", "display", "level", "device", "file", "file-list", "unit", "unit-list"];
+
+let op;
+if (argv['device-list']) {
+    setupDevice.showDeviceListAndExit();
+} else if (argv.version) {
+    version.showVersionAndExit();
+} else if (argv.help) {
+    help.display(processName, appdata.getConfig(true).profile);
+    cliControl.end();
+} else if (argv['current-daemon']) {
+    op = checkCurrentDaemon;
+} else if (argv['switch-daemon']) {
+    op = switchDaemon;
+} else if (argv['unit-list']) {
+    op = showUnitList;
+} else if (argv['file-list'] || argv.file) {
+    op = readMode;
+} else {
+    op = showLog;
+}
+if (op) {
+    version.checkNodeVersion(function(err) {
+        if (err)
+            return finish(err);
+        async.series([
+            op.bind(this)
+        ],finish);
+    });
+}
+
+function showLog() {
+    log.info("ares-log#printLog");
+
+    checkOption();
+    logLib.show(options, finish);
+}
+
+function readMode() {
+    log.info("ares-log#readMode");
+
+    checkOption();
+    logLib.readMode(options, finish);
+}
+
+function showUnitList() {
+    log.info("ares-log#showUnitList");
+
+    checkOption();
+    logLib.printUnitList(options, finish);
+}
+
+function checkCurrentDaemon() {
+    log.info("ares-log#checkCurrentDaemon");
+
+    options.currentDaemon = appdata.getConfig(true).logDaemon;
+    return finish(null, "Current log daemon is " + options.currentDaemon);
+}
+
+function switchDaemon() {
+    log.info("ares-log#switchDaemon");
+
+    if (argv['switch-daemon'] === "true") {
+        return finish(errHndl.getErrMsg("NOT_EXIST_DAEMONNAME"));
+    }
+    
+    // to-do: Input only in case of pmlogd, journald, and other error processing
+    // to-do: Write to the changed daemon in the config file
+
+    return finish(null, "Switched log daemon to " + argv['switch-daemon']);
+}
+
+function checkOption() {
+    log.info("ares-log#checkOption");
+
+    options.currentDaemon = appdata.getConfig(true).logDaemon;
+    options.currentOption = Object.keys(argv);
+    options.currentOption.splice(-1, 1);
+
+    if (options.currentDaemon === "journald") {
+        log.info("journald options");
+
+        options.currentOption.forEach(function(item){
+            if (!journalLogOptions.includes(item)) {
+                return finish(errHndl.getErrMsg("NOT_SUPPORT_JOURNALD", item));
+            }
+        });
+    } else if (options.currentDaemon === "pmLogd") {
+        log.info("pmlogd options");
+
+        options.currentOption.forEach(function(item){
+            if(!pmLogOptions.includes(item)) {
+                return finish(errHndl.getErrMsg("NOT_SUPPORT_PMLOGD", item));
+            }
+        });
+    }
+}
+
+function finish(err, value) {
+    if (err) {
+        // handle err from getErrMsg()
+        if (Array.isArray(err) && err.length > 0) {
+            for(const index in err) {
+                log.error(err[index].heading, err[index].message);
+            }
+            log.verbose(err[0].stack);
+        } else {
+            // handle general err (string & object)
+            log.error(err.toString());
+            log.verbose(err.stack);
+        }
+        cliControl.end(-1);
+    } else {
+        log.info('finish():', value);
+        if (value) {
+            console.log(value);
+        }
+        cliControl.end();
+    }
+}
---
+++ spec/jsSpecs/ares-log.spec.js
@@ -0,0 +1,385 @@
+/*
+ * Copyright (c) 2020 LG Electronics Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* eslint-disable no-useless-escape */
+
+const exec = require('child_process').exec,
+    path = require('path'),
+    fs = require('fs'),
+    common = require('./common-spec');
+
+const aresCmd = 'ares-log',
+    savedlogPath = path.join(__dirname, "..", "tempFiles", "savedlog.log"),
+    exp = /\w+ \d+ \d\d:\d\d:\d\d [\w|\d]+ [\w|\d|\.|\-]+\[\d+]:/g;
+
+let cmd,
+    options;
+
+beforeAll(function (done) {
+    cmd = common.makeCmd(aresCmd);
+    common.removeOutDir(savedlogPath);
+    common.getOptions()
+    .then(function(result){
+        options = result;
+        done();
+    });
+});
+
+afterAll(function (done) {
+    common.removeOutDir(savedlogPath);
+    done();
+});
+
+describe(aresCmd + " -h -v", function() {
+    it("Print help message with verbose log", function(done) {
+        exec(cmd + " -h -v", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+                expect(stderr).toContain("verb argv");
+            }
+            expect(stdout).toContain("SYNOPSIS");
+            expect(error).toBeNull();
+            done();
+        });
+    });
+});
+
+describe(aresCmd, function() {
+    it("Add device with ares-setup-device", function(done) {
+        common.resetDeviceList()
+        .then(function(){
+            return common.addDeviceInfo();
+        }).then(function(result) {
+            expect(result).toContain(options.device);
+            done();
+        }).catch(function(err) {
+            expect(err).toContain("The specified value already exist");
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " --device-list(-D)", function() {
+    it("Show available device list", function(done) {
+        exec(cmd + " -D", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain(options.device);
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -cd", function() {
+    it("Print current log daemon", function(done) {
+        exec(cmd + " -cd", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain("Current log daemon is journald");
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -sd", function() {
+    it("Print switch log daemon", function(done) {
+        exec(cmd + " -sd journald", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain("Switched log daemon to journald");
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -n 2", function() {
+    it('Show log with --lines option', function(done) {
+        exec(cmd + " -n 2", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain("-- Logs begin at");
+            expect(stdout.match(exp).length).toBe(2);
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -fl", function() {
+    it("Print .journal log file list", function(done) {
+        exec(cmd + " -fl", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain("system.journal");
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " --file", function() {
+    it("Show log with --file option", function(done) {
+        exec(cmd + " --file system.journal", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain("-- Logs begin at");
+            expect(stdout.match(exp).length > 0).toBeTrue();
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -ul", function() {
+    it("Print unit list", function(done) {
+        exec(cmd + " -ul", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain("bootd.service");
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -ul -dp 1", function() {
+    it("Print unit list with dp option", function(done) {
+            exec(cmd + " -ul -dp 1", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+                expect(stderr).toContain("[Tips]: This device does not support multiple sessions");
+            } else {
+                expect(stdout).toContain("sam.service");
+            }
+            done();
+        });
+    });
+});
+
+describe(aresCmd + "-n 2 -s logfile", function() {
+    it("Save log to file", function(done) {
+        exec(cmd + ` -n 2 -s ${savedlogPath}`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(fs.existsSync(savedlogPath)).toBe(true);
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -n 1 -o json", function() {
+    it("Show log with output option", function(done) {
+        exec(cmd + " -n 1 -o json", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(typeof JSON.parse(stdout)).toBe('object');
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -k", function() {
+    it("Show kenel log with --kernel option", function(done) {
+            exec(cmd + " -k", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            } else {
+                expect(stdout).toContain("-- Logs begin at");
+            }
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -b", function() {
+    it("Show boot log with --boot option", function(done) {
+            exec(cmd + " -b", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            } else {
+                expect(stdout).toContain("-- Logs begin at");
+            }
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " --pid", function() {
+    let pid;
+    it("Get a pid from log", function(done) {
+        const pidExp = /\w+ \d+ \d\d:\d\d:\d\d [\w|\d]+ [\w|\d|\.|\-]+\[(\d+)]:/;
+            exec(cmd + " -n 1", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            } else {
+                expect(stdout).toContain("-- Logs begin at");
+                expect(stdout.match(pidExp).length > 0).toBeTrue();
+                pid = stdout.match(pidExp)[1];
+                done();
+            }
+        });        
+    });
+    it("Show log with --pid option", function(done) {
+            exec(cmd + ` --pid ${pid} -n 3`, function (error, stdout, stderr) {
+            const expectedPid = `[${pid}]`;
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            } else {
+                expect(stdout).toContain("-- Logs begin at");
+                expect(stdout.match(expectedPid).length > 0).toBeTrue();
+                done();
+            }
+        });
+    });
+});
+
+describe(aresCmd + " --unit memorymanager", function() {
+    it("Install sample ipk to device with ares-install", function(done) {
+        const installCmd = common.makeCmd("ares-install");
+        exec(installCmd + ` ${options.ipkPath}`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain("Success", stderr);
+            done();
+        });
+    });
+
+    it("Launch sample App", function(done) {
+        const launchCmd = common.makeCmd("ares-launch");
+        exec(launchCmd + ` ${options.pkgId}`, function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            }
+            expect(stdout).toContain(`Launched application ${options.pkgId}`, error);
+            done();
+        });
+    });
+
+    it("Show log with --unit option", function(done) {
+            exec(cmd + " --unit memorymanager", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            } else {
+                const unitExp = /\w+ \d+ \d\d:\d\d:\d\d [\w|\d]+ memorymanager/g;
+                expect(stdout).toContain("-- Logs begin at");
+                expect(stdout.match(unitExp).length > 0).toBeTrue();
+                done();
+            }
+        });
+    });
+});
+
+describe(aresCmd +" -u sam -dp 1", function() {
+    it("Show log with unit and dp option", function(done) {
+            exec(cmd + " -u sam -dp 1", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+                expect(stderr).toContain("[Tips]: This device does not support multiple sessions");
+            } else {
+                const unitExp = /\w+ \d+ \d\d:\d\d:\d\d [\w|\d]+ sam/g;
+                expect(stdout).toContain("-- Logs begin at");
+                expect(stdout.match(unitExp).length > 0).toBeTrue();
+            }
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -S today", function() {
+    it("Show log with --since option", function(done) {
+            exec(cmd + " -S today", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            } else {
+                expect(stdout).toContain("-- Logs begin at");
+            }
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -U yesterday", function() {
+    it("Show log with --until option", function(done) {
+            exec(cmd + " -U yesterday", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+            } else {
+                expect(stdout).toContain("-- Logs begin at");
+            }
+            done();
+        });
+    });
+});
+
+// negative tc
+describe(aresCmd + " -aaa", function() {
+    it("Not support option", function(done) {
+            exec(cmd + " -aaa", function (error, stdout, stderr) {
+            if (stderr && stderr.length > 0) {
+                common.detectNodeMessage(stderr);
+                expect(stderr).toContain("[Tips]: Journal daemon do not suppport the option <aaa>");
+            }
+            done();
+        });
+    });
+});
+
+describe(aresCmd + " -f", function() {
+    it("Show log with --follow option", function(done) {
+        const child = exec(cmd + " -f");
+        let result = "";
+
+        child.stdout.on("data", function (data) {
+            result += data;
+        });
+
+        child.stderr.on("data", function (data) {
+            if (data && data.length > 0) {
+                common.detectNodeMessage(data);
+            }
+            expect(data).toBeNull();
+        });
+
+        setTimeout(() => {
+            child.kill();
+            expect(result).toContain("-- Logs begin at");
+            expect(result.match(exp).length > 0).toBeTrue();
+            done();
+        }, 1000);
+    });
+});
+
+describe(aresCmd + " -r", function() {
+    it("Show log with --reverse option", function(done) {
+        const child = exec(cmd + " -r");
+        let result = "";
+
+        child.stdout.on('data', function (data) {
+            result += data;
+        });
+
+        child.stderr.on('data', function (data) {
+            if (data && data.length > 0) {
+                common.detectNodeMessage(data);
+            }
+            expect(data).toBeNull();
+        });
+
+        setTimeout(() => {
+            child.kill();
+            expect(result).toContain('-- Logs begin at');
+            expect(result.match(exp).length > 0).toBeTrue();
+            done();
+        }, 1000);
+    });
+});
+
